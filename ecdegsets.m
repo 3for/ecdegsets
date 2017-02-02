03467352134632423b7cb5aa622df10d9ee65bb3
oronpropp <oronpropp@gmail.com>
2017-02-01 20:21:28 -0500

    Update ecdegsets.m

--- ecdegsets.m
+++ ecdegsets.m
@@ -1,7 +1,7 @@
 /*
     Magma code related to the paper:
     [1] "Cartan images and l-torsion points of elliptic curves with rational
-        j-invariant."
+        j-invariant," https://arxiv.org/abs/1702.00121.
     Copyright (c) 2017 by Oron Y. Propp.
     You are welcome to use and/or modify this code under the terms of version 3
     of the GNU General Public License or any later version (but please cite the
